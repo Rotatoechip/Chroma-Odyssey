17cafbe771bd8467740aa10d3fadf5759b1b5455
Lawrenceony <lawrenceony@gmail.com>
2024-04-04 00:38:21 -0400

    Transition to Level 2
    Added a new script attached to the checkpoint in level 1 that will allow the player change to the new scene (level 2) once they reach it.

---
+++ Chroma Odyssey/Assets/LevelTransition.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if the colliding object is the player
+        if (collision.CompareTag("Player"))
+        {
+            // Load "Level 2"
+            SceneManager.LoadScene("Level 2");
+        }
+    }
+}
